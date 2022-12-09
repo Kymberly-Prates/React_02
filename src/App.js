7b79057031e2cc58eb772bf6ae56d7bcf35f5c0c
Kymberly-Prates <112019085+Kymberly-Prates@users.noreply.github.com>
2022-12-09 15:29:14 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import {useState, useEffect, useRef} from 'react'
 import './App.css';
+import { motion } from 'framer-motion'
 
-function App() {
+import image1 from '../src/img/1.jpeg'
+import image2 from '../src/img/2.jpeg'
+import image3 from '../src/img/3.jpeg'
+import image4 from '../src/img/4.jpeg'
+import image5 from '../src/img/5.jpeg'
+import image6 from '../src/img/6.jpeg'
+import image7 from '../src/img/7.jpeg'
+import image8 from '../src/img/8.jpeg'
+
+const img = [image1, image2, image3,image4,image5,image6,image7,image8]
+
+function App(){
+  const carousel = useRef();
+  const [width,setWidth] = useState(0)
+
+  useEffect(() => {
+    console.log(carousel.current?.scrolWidh, carousel.current?.offsetWidt)
+    setWidth(carousel.corrent?.scrolWidh - carousel.current?.offsetWidt)  
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+
+      <motion.div ref={carousel} className='carousel'whileTap={{cursor: "grabbing"}}>
+        <motion.div className='inner'
+        drag="x"
+        dragConstraints={{right: 0,left: -width}}
+        inicial={{x: 100}}
+        animate={{x:0}}
+        transition={{duration:0.8}}
         >
-          Learn React
-        </a>
-      </header>
+
+        {img.map(image => (
+          <motion.div className="item" key={image}>
+            <img src={image} alt="Texto alt"/>
+          </motion.div>  
+        ))}
+
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
